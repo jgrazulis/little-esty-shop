b8582e48913ad2df2f38b998777742a49c2a3173
Sierra Tucker <sierra.tucker121@gmail.com>
2022-01-11 19:52:18 -0700

    Update: add partials for links at top of every page

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,6 +7,7 @@ class ItemsController < ApplicationController
   def show
     @item = Item.find(params[:id])
     @merchant = @item.merchant
+    @merchant = Merchant.find(params[:merchant_id])
   end
 
   def update
